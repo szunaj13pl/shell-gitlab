1ed1419ca544681b7e3026e0a65a35bd692a3729
Janusz Ładecki <janusz.ladecki@3s.pl>
2018-01-22 20:12:13 +0100

    (!) Change - Update instalation script

--- install.sh
+++ install.sh
@@ -1,9 +1,32 @@
-#!/bin/bash
-
 # Janusz Ładecki <szunaj13pl@gmail.com>
 # Created on 22.01.2018
 
 function install_gitlab() {
+    
+    # Use colors, but only if connected to a terminal, and that terminal
+    # supports them.
+    if which tput >/dev/null 2>&1; then
+        ncolors=$(tput colors)
+    fi
+    if [ -t 1 ] && [ -n "$ncolors" ] && [ "$ncolors" -ge 8 ]; then
+        RED="$(tput setaf 1)"
+        GREEN="$(tput setaf 2)"
+        YELLOW="$(tput setaf 3)"
+        BLUE="$(tput setaf 4)"
+        BOLD="$(tput bold)"
+        NORMAL="$(tput sgr0)"
+    else
+        RED=""
+        GREEN=""
+        YELLOW=""
+        BLUE=""
+        BOLD=""
+        NORMAL=""
+    fi
+    
+    # Only enable exit-on-error after the non-critical colorization stuff,
+    # which may fail on systems lacking tput or terminfo
+    set -e
     
     # Create tempormaly folder for clean instalation
     
